533e8537aa36c6196ff0934abe1b275a1441667d
Victor Magdaleno <vmagdaleno55@gmail.com>
2021-01-08 20:34:22 -0800

    Chapter 2: 	Numpy --> NumPy Chapter 3: 	Links to original documentation in each section 	explanation of gene dataset 	examples are all modular

--- _build/jupyter_execute/Chapter_2/NUMPY.py
+++ _build/jupyter_execute/Chapter_2/NUMPY.py
@@ -3,9 +3,11 @@
 
 # # NumPy
 
-# In this chapter we will be discussing the advatages to using the *NumPy* package to analyze large datasets. Numpy is a useful tool that can help store and wrangle homogeneous data. This means data that the data are of the same data type such as all `floats` or all `integers`.
+#  NumPy is a useful package that can help store and wrangle homogeneous data. This means data that the data are of the same [data type](https://jakevdp.github.io/PythonDataScienceHandbook/02.01-understanding-data-types.html) such as all **floats** or all **integers**.
+# 
+# We strongly recommend working through the ["NumPy Quickstart Tutorial"](https://numpy.org/doc/stable/user/quickstart.html) for a more comprehensive introduction to NumPy. Here, we'll introduce some useful tools using the *NumPy* package to analyze large datasets.
 
-# Before we can use numpy, we need to import the package. We can also nickname the modules when we import them. The convention is to import `numpy` as `np`. 
+# Before we can use NumPy, we need to import the package. We can also nickname the modules when we import them. The convention is to import `numpy` as `np`. 
 
 # In[1]:
 
@@ -19,7 +21,7 @@ get_ipython().run_line_magic('whos', '')
 
 # ## NumPy Arrays
 
-# The basis of the NumPy package is the numpy **array**. A numpy array is similar to a list of lists or a grid of values. You can create a numpy array from lists using `np.array()`, by reading files, or through functions built into the numpy package such as such as `arange`, `linspace`, `empty`, which we will discuss later. 
+# The basis of the NumPy package is the **array**. A NumPy array is similar to a list of lists or a grid of values. You can create a [NumPy array](https://numpy.org/doc/stable/reference/generated/numpy.array.html) from a list using `np.array()`, by reading in a file, or through functions built into the NumPy package such as such as `arange`, `linspace`, `empty`, which we will discuss later. 
 
 # In[2]:
 
@@ -28,7 +30,7 @@ get_ipython().run_line_magic('whos', '')
 list1 = [2, 4, 6, 8, 10, 12]
 
 # Store list as a numpy array 
-array1 = np.array([list1])
+array1 = np.array(list1)
 print(array1)
 
 
@@ -40,7 +42,7 @@ print(array1)
 # Create a 2nd random list 
 list2 = [1, 3, 5, 7, 9, 11]
 
-# Store list of lists as a numpy array 
+# Store list of lists as a NumPy array 
 array1 = np.array([list1, list2])
 print(array1)
 
@@ -73,15 +75,9 @@ print(array1.dtype)
 
 # ## Indexing & Slicing Arrays
 
-# You can index Numpy arrays using `array_name[row,column]` to select a single value. If you omit the column, it will give you the entire row. You can also use `:` for either row or column and it will return all of those values. We will demonstrate by indexing into `array1`. 
+# You can index NumPy arrays using `array_name[row,column]` to select a single value. If you omit the column, it will give you the entire row. You can also use `:` for either row or column and it will return all of those values. We will demonstrate by indexing into `array1`. 
 
 # In[6]:
-
-
-print(array1)
-
-
-# In[7]:
 
 
 # Select the number 6 from our array 
@@ -93,9 +89,9 @@ print('The values stored in row 2 are:')
 print(array1[1])
 
 
-# You may want to look at a slice of cloumns or a slice of rows. You can do so using `start_index:stop_index` for either row or columns to select your slice.
+# You may want to look at a slice of columns or a slice of rows. You can do so using `array(start_index:stop_index)` for either row or columns to select your slice.
 
-# In[8]:
+# In[7]:
 
 
 # Print the first 3 columns of each row 
@@ -104,7 +100,7 @@ print(array1[: ,0:3])
 
 # You can also select multiple specifc columns by inputing a `list` as your `columns`. Lets try to index the first, third, and last column in `array1`.
 
-# In[9]:
+# In[8]:
 
 
 # Choose your columns of interest 
@@ -114,7 +110,7 @@ print(array1[:, columns])
 
 # We can also change values in an array similar to how we would change values in a list. The syntax we use is `array[row,column] = new_desired_value`.
 
-# In[10]:
+# In[9]:
 
 
 # Change the entire first row of array1 to 100
@@ -122,17 +118,17 @@ array1[0,:] = 100
 print(array1)
 
 
-# ## Boolean Indexing 
+# ## Subsetting 
 
-# We can also index arrays using booleans. When we use Booleans, we can think of this as *filtering* the array by applying a condition to our array. The syntax for this type of indexing would be `new_array = original_array[condition]`.
+# We can also subet our original array to only include data that meets our criteria. We can think of this as *filtering* the array by applying a condition to our array. The syntax for this would be `new_array = original_array[condition]`.
 
-# In[11]:
+# In[10]:
 
 
 # Reassign our original array 
 array1 = np.array([list1, list2])
 
-# Index values greater than 5 from our array 
+# Return values greater than 5 from our array 
 condition = (array1 > 5)
 filtered_array = array1[condition]
 print(filtered_array)
@@ -140,9 +136,9 @@ print(filtered_array)
 
 # ## Benefits of Using Arrays 
 
-# The list of lists format is not wrong per say, but it makes working with the data more difficult. For example, if you were trying to add the numbers of the two lists together, simply adding the lists would only append one list at the end of the other. However, if you add two numpy arrays together, the values of voth arrays will be summed. 
+# The list of lists format is not wrong per say, but it makes working with the data more difficult. For example, if you were trying to add the numbers of the two lists together, simply adding the lists would only append one list at the end of the other. However, if you add two NumPy arrays together, the values of both arrays will be summed. 
 
-# In[12]:
+# In[11]:
 
 
 # Add two lists together 
@@ -159,7 +155,7 @@ print(array2 + array3)
 
 # Alternitavely, you can use the `sum()` method to add all values in an array together. You can also specify whether you want to sum the values across rows or columns in a grid/matrix. If you specify you want to sum values in rows or columns, the output will be an array of the sums. 
 
-# In[13]:
+# In[12]:
 
 
 # Create a 2 by 3 array 
@@ -185,23 +181,23 @@ print(array4.sum(axis = 1))
 
 # ### NumPy also includes some very useful array generating functions:
 # 
-# * `arange`: like `range` but gives you a useful numpy array, instead of an interator, and can use more than just integers)
+# * `arange`: like `range` but gives you a useful NumPy array, instead of an interator, and can use more than just integers)
 # * `linspace` creates an array with given start and end points, and a desired number of points
 # * `logspace` same as linspace, but in log.
 # * `random` can create a random list (there are <a href="https://docs.scipy.org/doc/numpy-1.14.0/reference/routines.random.html">many different ways to use this</a>)
 # * `concatenate` which can concatenate two arrays along an existing axis [<a href="https://docs.scipy.org/doc/numpy/reference/generated/numpy.concatenate.html">documentation</a>]
 # * `hstack` and `vstack` which can horizontally or vertically stack arrays
 # 
-# Whenever we call these, we need to use whatever name we imported numpy as (here, `np`). We will demonstrate some of these functions below.
+# Whenever we call these, we need to use whatever name we imported `numpy` as (here, `np`). We will demonstrate some of these functions below.
 
-# In[14]:
+# In[13]:
 
 
 # When using linspace, both end points are included
 print(np.linspace(0,147,10))
 
 
-# In[15]:
+# In[14]:
 
 
 # First array is  a list of 10 numbers that are evenly spaced, 
@@ -216,7 +212,7 @@ print(first_array)
 print(second_array)
 
 
-# In[16]:
+# In[15]:
 
 
 # Create an array that has two rows 
@@ -228,16 +224,19 @@ print(big_array)
 
 # Numpy also has built in methods to save and load arrays: `np.save()` and `np.load()`. Numpy files have a .npy extension. See full documentation <a href="https://docs.scipy.org/doc/numpy/reference/generated/numpy.save.html">here</a>.
 
-# In[17]:
+# In[16]:
 
 
 # Save method takes arguments 'filename' and then 'array':
 np.save('big_array',big_array)
 
 
-# In[18]:
+# In[17]:
 
 
 my_new_matrix = np.load('big_array.npy')
 print(my_new_matrix)
 
+
+# ## Additional Resources
+# See the [Python Data Science Handbook](https://jakevdp.github.io/PythonDataScienceHandbook/02.00-introduction-to-numpy.html) for a more in depth exploration of NumPy, and of course, the original documentation.
--- _build/jupyter_execute/Chapter_3/Working_with_NWB_format_in_Python.py
+++ _build/jupyter_execute/Chapter_3/Working_with_NWB_format_in_Python.py
@@ -3,10 +3,153 @@
 
 # # Working with NWB in Python 
 
-# *Content to be added in later*
+# ## About NWB
+
+# - many obstacles in data sharing, especially in neuroscience 
+# - there is currently too much data in neuroscience to work with 
+# - too much data makes it difficult to share and analyze with scientisits around the worls 
+# - "The aim of Neurodata Without Borders is to standardize neuroscience data on an international scale"
+# - creating a standard format for neuroscience data will mamke it easier to share and contribute to open soruce projects which will accelerate discovery 
+
+# ## Goal
+
+# - develop a unified, extensible, open-source data format for cellular-based neurophysiology data
+# - create a standard for neuroscience datasets to imporve the collaboration of neuroscience projects around the world 
+
+# ## Current Conflict 
+
+# - neuroscience does not have a standardized way to collect and share data
+# - no common standard means it is difficult for labs to share and compare results with one another 
+# - not being able to compare results and replicate experiments slows down the overall progression of the field 
+
+# ## Neurodata Without Borders: Neurophysiolgy 
+
+# - a common standard to share, store, and build analysis tools for neuroscience data 
+# - contains software to stadardize data, API's to read and write data, and important datasets in the neuroscience community that have been converted to NWB format 
+# - takes into account experimental design, experimental subjects, behavior, data aquisition, neural activity, and extensions 
+
+# ## NWB Tutorials: Object IDs 
+
+# In[1]:
+
+
+from pynwb import NWBFile, TimeSeries
+from datetime import datetime
+from dateutil.tz import tzlocal
+import numpy as np
+
+# set up the NWBFile
+start_time = datetime(2019, 4, 3, 11, tzinfo=tzlocal())
+nwbfile = NWBFile(session_description='demonstrate NWB object IDs',
+                  identifier='NWB456',
+                  session_start_time=start_time)
+
+# make some fake data
+timestamps = np.linspace(0, 100, 1024)
+data = np.sin(0.333 * timestamps) + np.cos(0.1 * timestamps) + np.random.randn(len(timestamps))
+test_ts = TimeSeries(name='raw_timeseries', data=data, unit='m', timestamps=timestamps)
+
+# add it to the NWBFile
+nwbfile.add_acquisition(test_ts)
+
+# print the object ID of the NWB file
+print(nwbfile.object_id)
+
+# print the object ID of the TimeSeries
+print(test_ts.object_id)
+
+
+# In[2]:
+
+
+nwbfile.objects
+
+
+# In[3]:
+
+
+for oid in nwbfile.objects:
+    print(nwbfile.objects[oid])
+
+
+# In[4]:
+
+
+ts_id = test_ts.object_id
+my_ts = nwbfile.objects[ts_id]  # test_ts == my_ts
+my_ts
+
+
+# In[5]:
+
+
+for obj in nwbfile.objects.values():
+    print('%s: %s "%s"' % (obj.object_id, obj.neurodata_type, obj.name))
+
+
+# ## NWB Tutorials: NWB Basics 
+
+# In[6]:
+
+
+# The NWB file 
+start_time2 = datetime(2017, 4, 3, 11, tzinfo=tzlocal())
+create_date2 = datetime(2017, 4, 15, 12, tzinfo=tzlocal())
+
+nwbfile2 = NWBFile(session_description='demonstrate NWBFile basics',  # required
+                  identifier='NWB123',  # required
+                  session_start_time=start_time2,  # required
+                  file_create_date=create_date2)  # optional
+print(nwbfile2)
+
+
+# In[7]:
+
+
+# Time Series Data
+from pynwb import TimeSeries
+
+data2 = list(range(100, 200, 10))
+timestamps2 = list(range(10))
+test_ts2 = TimeSeries(name='test_timeseries', data=data2, unit='m', timestamps=timestamps2)
+print(timestamps2)
+print(test_ts2)
+
+
+# In[8]:
+
+
+# Uniform Sampled rate 
+rate_ts = TimeSeries(name='test_timeseries', data=data2, unit='m', starting_time=0.0, rate=1.0)
+rate_ts
+
+
+# In[9]:
+
+
+nwbfile2.add_acquisition(test_ts)
+
+
+# In[10]:
+
+
+nwbfile2
+
+
+# In[11]:
+
+
+nwbfile2.acquisition['test_timeseries']
+
+
+# In[28]:
+
+
+nwbfile2.objects
+
 
 # In[ ]:
 
 
-
+# Writing an NWB file 
 
